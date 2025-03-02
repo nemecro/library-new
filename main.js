428ddb5957e43a60b0a45da3fd3735ef0276898b
nemecro <nemecstudies@gmail.com>
2025-03-02 21:02:45 +0100

    create an add book modal with a form, when submitted add book to the library array and render it in the table

--- main.js
+++ main.js
@@ -44,7 +44,36 @@ function displayBooks(){
     table.append(tableBody);
 };
 
+/* REMOVE LATER */
 addBook('War and Peace', 'Tolstoy', '500', 'read');
 addBook('Crime and Punishment', 'Dostoevsky', '750', 'not-read');
 
 displayBooks();
+/* END */
+
+const addBtn = document.querySelector('#add-btn');
+const addDialog = document.querySelector('#add-dialog');
+const form = document.querySelector('#form-dialog');
+const closeBtn = document.querySelector('#close-btn');
+const confirmBtn = document.querySelector('#confirm-btn');
+
+addBtn.addEventListener('click', () =>{
+    addDialog.showModal();
+});
+
+closeBtn.addEventListener('click', () => {
+    addDialog.close();
+});
+
+confirmBtn.addEventListener('click', () => {
+    const formData = Object.fromEntries(new FormData(form));
+    addBook(formData.title, formData.author, formData.pages, formData.status);
+    displayBooks();
+});
+
+addDialog.showModal();
+
+
+
+
+
